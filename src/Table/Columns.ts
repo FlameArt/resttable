9641d172140e55ab5264fcccae715f5cc3feab87
Flame <flameartme@gmail.com>
2023-11-10 17:36:26 +0400

    fix: ts

--- src/Table/Columns.ts
+++ src/Table/Columns.ts
@@ -41,7 +41,7 @@ export class Column implements IColumn {
      * @param col 
      * @returns 
      */
-    titleCustom: (col: Column) => null,
+    titleCustom: null,
 
     /**
      * Выделена ли строка чекбоксом
@@ -311,7 +311,7 @@ export interface IColumn {
      * @param col 
      * @returns 
      */
-    titleCustom: (col: Column) => string | null,
+    titleCustom: ((col: Column) => string) | null,
 
   },
 
